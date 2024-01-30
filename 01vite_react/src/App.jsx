5d152a829c82b23dc6e7ab8769509b29a1a00610
Vinay14Newbie <wjust1984@gmail.com>
2024-01-30 19:35:48 +0530

    learn file structure and create own react library

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,6 +1,12 @@
+import Chai from "./chai"
+
 function App() {
   return (
+    // we cannot use return multiple elements at once so use </> this called as fragment in React
+    <>
       <h1> Chai aur Code with vite | Vinay</h1>
+      <Chai/>
+    </>
   )
 }
 
---
+++ 01vite_react/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>React is ready</h3>
+    )
+}
+
+export default Chai
