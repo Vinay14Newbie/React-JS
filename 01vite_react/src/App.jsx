e039ca09106c3712c10b2e934311ed0936a7ff5a
Vinay14Newbie <wjust1984@gmail.com>
2024-01-30 20:31:09 +0530

    custom react working

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,10 +1,12 @@
 import Chai from "./chai"
 
 function App() {
+  const username = 'chai aur code'
+
   return (
     // we cannot use return multiple elements at once so use </> this called as fragment in React
     <>
-      <h1> Chai aur Code with vite | Vinay</h1>
+      <h1> Chai aur Code with vite | Vinay {username}</h1> {/* {username} = evaluated expression, we write final outcome of js here */}
       <Chai/>
     </>
   )
--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+const anotherEle = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = ' Chai aur Code'
+
+
+/*
+  Built in properties that belong on the element
+  type,
+  key,
+  ref,
+  props
+*/
+const reactEle = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <MyApp/>
+  // MyApp()  //we can also use this but use above 
+
+  // anotherEle   
+  reactEle
+
+  // <App/>
 )
