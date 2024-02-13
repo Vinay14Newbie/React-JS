e3158ca2050227cd70c475e4a287b13efd29bd78
Vinay14Newbie <wjust1984@gmail.com>
2024-02-13 19:54:36 +0530

    update

---
+++ 7-Currency_Converter/src/components/InputBox.jsx
@@ -0,0 +1,56 @@
+import React, {useId} from "react";
+
+function InputBox({
+    label,   // from | to
+    amount,  // total amount
+    onAmountChange, 
+    onCurrencyChange, 
+    currencyOptions=[], 
+    selCurrency="usd", 
+    amountDisable=false,  
+    currencyDisable=false, 
+    className = ""}) {
+
+    // useId is a React Hook for generating unique IDs that can be passed to accessibility attributes.
+    const amountInputId = useId()
+
+    return (
+        // js css | if at any case user wants to set the css
+        <div className={`bg-white p-3 rounded-lg text-lg flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
+                    {label}
+                </label>
+                <input
+                    id={amountInputId}
+                    className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"  
+                    value={selCurrency}
+                    onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+                >
+                       
+                    {currencyOptions.map((currency)=>(
+                        // remember the key in react 
+                        <option key={currency} value={currency}>   
+                            {currency}
+                        </option>
+                    ))}
+
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox;
--- 7-Currency_Converter/src/custom_hooks/useCurrencyInfo.js
+++ 7-Currency_Converter/src/custom_hooks/useCurrencyInfo.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    
     useEffect(() => {
-        const [data, setData] = useState({})
 
         // the api given below is pretty interesting, when we give 'inr' in place of currency we can see all the conversion rate of inr -> any currency
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((response) => response.json())
         .then((response) => setData(response[currency]))
     }, [currency])
-    console.log(data);
     return data
 }
 
-// export default useCurrencyInfo
+export default useCurrencyInfo;
